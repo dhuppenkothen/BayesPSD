69220a1224b2ae83f1b81c7f48687dd4e33c31e7
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2015-06-13 15:17:28 -0400

    Added initialization files for package

---
+++ __init__.py
@@ -0,0 +1,11 @@
+## Python Init File
+
+from code.lightcurve import Lightcurve
+from code.powerspectrum import PowerSpectrum
+from code.posterior import Posterior, PerPosterior, StackPerPosterior
+from code.bayes import Bayes
+from code.mcmc import MarkovChainMonteCarlo,MetropolisHastings
+from code.burst import Burst,GBMBurst
+from code.mle import *
+from code.utils import *
+
